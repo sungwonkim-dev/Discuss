b3c7d4194f1e67ef88cb0a96c4c2538e4cdb4ae5
sungwonkim <sungwon5047@gmail.com>
2019-04-15 22:37:05 +0900

    [source add] create project discuss-scraper

---
+++ discuss-scraper/src/main/java/com/haja/discussscraper/job/scrap/ScrapJobConfiguration.java
@@ -0,0 +1,37 @@
+package com.haja.discussscraper.job.scrap;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class ScrapJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job testJob() {
+        return jobBuilderFactory.get("testJob")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory.get("step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(" discuss scraper step");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
