9594f137deb6c3559f2c58541e9cd4b7ffd6d5d2
sungwonkim <sungwon5047@gmail.com>
2019-06-05 14:50:19 +0900

    [source commit]  findIdByUid 추가.

--- discuss/src/main/java/com/haja/discuss/web/login/repository/UserRepository.java
+++ discuss/src/main/java/com/haja/discuss/web/login/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long> {
     User    findByUid(String uid);
     boolean existsByUid(String uid);
-
+    Long findIdByUid(String uid);
 
 }
