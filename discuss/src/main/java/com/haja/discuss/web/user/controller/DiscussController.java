411bdaa0f1037089047df72e849586bc452cbb91
sungwonkim <sungwon5047@gmail.com>
2019-06-05 14:52:54 +0900

    [source commit] DiscussController 기능 수정

--- discuss/src/main/java/com/haja/discuss/web/user/controller/DiscussController.java
+++ discuss/src/main/java/com/haja/discuss/web/user/controller/DiscussController.java
@@ -1,16 +1,19 @@
 package com.haja.discuss.web.user.controller;
 
+import com.haja.discuss.entity.Comment;
+import com.haja.discuss.entity.Discussion;
+import com.haja.discuss.entity.User;
 import com.haja.discuss.web.login.controller.CommonController;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.haja.discuss.web.login.service.UserService;
+import com.haja.discuss.web.user.service.DiscussService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import static com.haja.discuss.DiscussContants.*;
 
@@ -18,49 +21,113 @@ import static com.haja.discuss.DiscussContants.*;
 @RequestMapping(value = "/discuss")
 public class DiscussController extends CommonController {
 
+    @Autowired
+    DiscussService discussService;
+
+    @Autowired
+    UserService userService;
+
+
+    @RequestMapping("")
+    public String requestDiscussHomePage(ModelMap modelMap) {
+
+        putUserInfo(modelMap);
+
+        return VIEW_DISCUSS_HOME_PAGE;
+    }
+
     @RequestMapping("/live")
-    public String requestLiveDiscussPage() {
+    public String requestLiveDiscussPage(ModelMap modelMap) {
+
+        putUserInfo(modelMap);
+        List<Discussion> discussionList = discussService.findRecentlySavedDiscussionOrderByCreated();
+        modelMap.put("discussionList", discussionList);
+
         return VIEW_DISCUSS_LIVE_PAGE;
     }
 
     @RequestMapping("/interested")
-    public String requestInterestedDiscussPage() {
+    public String requestInterestedDiscussPage(ModelMap modelMap) {
+
+        putUserInfo(modelMap);
+        User user = getUserInCurContext();
+        List<Discussion> discussionList = discussService.findAllMyInterestingDiscussion(user);
+        modelMap.put("discussionList", discussionList);
+
         return VIEW_DISCUSS_INTERESTED_PAGE;
     }
 
     @RequestMapping("/debate")
-    public String requestDebateDiscussPage() {
+    public String requestDebateDiscussPage(@RequestParam(value = "pageNo", defaultValue = "1", required = false) String pageNo,
+                                           @RequestParam(value = "discussionCount", defaultValue = "1", required = false) String discussionCount,
+                                           @RequestParam(value = "size", defaultValue = "10", required = false) String size,
+                                           ModelMap modelMap) {
+
+        putUserInfo(modelMap);
+        List<Discussion> tempList = discussService.findAllDiscussionOrderByDid(pageNo, size);
+
+        modelMap.put("discussionList", tempList);
+        modelMap.put("pageNo", pageNo);
+        modelMap.put("discussionCount", discussService.getAllDiscussionSize());
+        modelMap.put("size", size);
+
         return VIEW_DISCUSS_DEBATE_PAGE;
     }
 
-    @RequestMapping("/free")
-    public String requestFreeDiscussPage() {
-        return VIEW_DISCUSS_FREE_PAGE;
-    }
-
     @RequestMapping("/rank")
-    public String requestRankPage() {
+    public String requestRankPage(ModelMap modelMap) {
+        putUserInfo(modelMap);
+
+        List<Discussion> discussionList = discussService.findTop10OrderByVisitedCount();
+        modelMap.put("discussionList", discussionList);
+
         return VIEW_DISCUSS_RANK_PAGE;
     }
 
     @RequestMapping("/write")
-    public String requestWritePage(@RequestParam(value = "text-editor", required = false) String text,
-                                   Model model) {
+    public String requestWritePage(ModelMap modelMap) {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(authentication.getPrincipal());
-
-        if (text != null) System.out.println(text);
+        putUserInfo(modelMap);
 
         return VIEW_DISCUSS_WRITE_PAGE;
     }
 
-    @RequestMapping("/encode-base64")
+    @RequestMapping("/save-content")
     @ResponseBody
-    public String encodeImageByBase64(HttpServletRequest request) {
+    public String saveContent(@RequestParam(value = "title", required = false) String title,
+                              @RequestParam(value = "text-editor", required = false) String content) {
 
-        String encodedImage = "";
+        Discussion discussion = discussService.saveDiscussionContent(getUserInCurContext(), title, content);
 
-        return encodedImage;
+        String did = EMPTY_DID;
+        if (discussion != null || discussion.getDid() != null)
+            did = String.valueOf(discussion.getDid());
+
+        return did;
+    }
+
+    @RequestMapping("/detail")
+    public String requestDetail(@RequestParam(value = "did", required = true) String did,
+                                ModelMap modelMap) {
+
+        putUserInfo(modelMap);
+        modelMap.put("discussion", discussService.findByDid(did));
+        modelMap.put("comments", discussService.findAllCommentByDid(did));
+        
+        return VIEW_DISCUSS_DETAIL_PAGE;
+    }
+
+    @RequestMapping("/save-comment")
+    @ResponseBody
+    public String saveComment(@RequestParam(value = "text-editor", required = false) String content,
+                              @RequestParam(value = "t-did") String did) {
+
+        Comment comment = discussService.saveDiscussionComment(did, content);
+
+        String callBackdid = EMPTY_DID;
+        if (comment != null || comment.getDid() != null)
+            callBackdid = String.valueOf(comment.getDid());
+
+        return callBackdid;
     }
 }
