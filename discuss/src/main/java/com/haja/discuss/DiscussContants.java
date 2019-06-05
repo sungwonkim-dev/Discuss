6dc8797d8c1c54f03f3e8ba52c248b8911fdb28b
sungwonkim <sungwon5047@gmail.com>
2019-06-05 15:04:26 +0900

    [source commit] 상수 클래스 정리

--- discuss/src/main/java/com/haja/discuss/DiscussContants.java
+++ discuss/src/main/java/com/haja/discuss/DiscussContants.java
@@ -1,23 +1,24 @@
 package com.haja.discuss;
 
 final public class DiscussContants {
-    public final static String VIEW_LOGIN_PAGE              = "/common/login";
-    public final static String VIEW_HOME_PAGE               = "/common/home";
-    public final static String VIEW_ERROR_PAGE              = "/common/error";
-    public final static String VIEW_CREATE_ACCOUNT_PAGE     = "/common/create-account";
+    public final static String VIEW_LOGIN_PAGE = "/common/login";
+    public final static String VIEW_HOME_PAGE = "/common/home";
+    public final static String VIEW_CREATE_ACCOUNT_PAGE = "/common/create-account";
+    public final static String VIEW_ACCESS_DENIED_PAGE = "/common/access-denied";
 
-
-    public final static String VIEW_DISCUSS_LIVE_PAGE       = "/discuss/live";
+    public final static String VIEW_DISCUSS_HOME_PAGE = "/discuss/home";
+    public final static String VIEW_DISCUSS_LIVE_PAGE = "/discuss/live";
     public final static String VIEW_DISCUSS_INTERESTED_PAGE = "/discuss/interested";
-    public final static String VIEW_DISCUSS_DEBATE_PAGE     = "/discuss/debate";
-    public final static String VIEW_DISCUSS_FREE_PAGE       = "/discuss/free";
-    public final static String VIEW_DISCUSS_RANK_PAGE       = "/discuss/rank";
-    public final static String VIEW_DISCUSS_WRITE_PAGE       = "/discuss/write";
+    public final static String VIEW_DISCUSS_DEBATE_PAGE = "/discuss/debate";
+    public final static String VIEW_DISCUSS_RANK_PAGE = "/discuss/rank";
+    public final static String VIEW_DISCUSS_WRITE_PAGE = "/discuss/write";
+    public final static String VIEW_DISCUSS_DETAIL_PAGE = "/discuss/detail";
 
-    public final static String ROLE_USER                    = "USER";
+    public final static String ROLE_USER = "USER";
 
-    public final static int    TRUE                         = 1;
-    public final static int    FALSE                         = 0;
+    public final static String EMPTY_DID = "0";
 
-    public final static int    FIXED_OFFSET                         = 10;
+    public final static int TRUE = 1;
+    public final static int FALSE = 0;
+
 }
