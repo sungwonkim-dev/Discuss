6f806a88b8b2b315d26fbb0e60e710c40e45c881
sungwonkim <sungwon5047@gmail.com>
2019-06-05 14:46:31 +0900

    [source commit] Base64 encode 추가

--- discuss/src/main/java/com/haja/discuss/util/EncodeUtils.java
+++ discuss/src/main/java/com/haja/discuss/util/EncodeUtils.java
@@ -1,4 +1,15 @@
 package com.haja.discuss.util;
 
+import java.util.Base64;
+
 public class EncodeUtils {
+    public static String encodeBase64(String content){
+        Base64.Encoder encoder = Base64.getEncoder();
+        return String.valueOf(encoder.encode(content.getBytes()));
+    }
+
+    public static String decodeBase64(String content){
+        Base64.Decoder decoder = Base64.getDecoder();
+        return String.valueOf(decoder.decode(content));
+    }
 }
